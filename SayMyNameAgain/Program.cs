76d8695a1d23634d6a6f8f63ab44e8118e131f68
kysonhensley <hensleykyson@gmail.com>
2025-09-13 15:20:06 -0600

    Add project files.

---
+++ SayMyNameAgain/Program.cs
@@ -0,0 +1,43 @@
+﻿//Kyson Hensley
+//RCET2265
+//Fall 2025
+//Hello World Program
+
+
+using System.ComponentModel.Design;
+
+namespace SayMyNameAgain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type your name.");
+
+            string name = Console.ReadLine();
+
+            
+            
+            if (name == "Emily")
+            {
+                Console.WriteLine($"Hello, {name}!");
+            }
+            else if (name == "Joe")
+            {
+                Console.WriteLine($"Hello, {name}!");
+            }
+            else if (name == "Kyson")
+            {
+                Console.WriteLine("I have the same name as you!");
+            }
+            else
+            {
+                Console.WriteLine($"Good Afternoon, {name}!");
+            }
+
+
+
+            Console.Read();
+        }
+    }
+}
