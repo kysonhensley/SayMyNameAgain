653e798f3f68a5eaf769cf7ec8e2f2b7e8e870f2
kysonhensley <hensleykyson@gmail.com>
2026-01-23 08:27:50 -0700

    Add project files.

---
+++ SayMyNameAgain/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SayMyNameAgain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            
+            Console.WriteLine("Please type your name:");
+            userInput = Console.ReadLine();
+
+        }
+    }
+}
