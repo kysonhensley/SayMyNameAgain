b51eeb5e3e816020a1055e5dbee4173127a0a170
kysonhensley <hensleykyson@gmail.com>
2026-01-27 17:13:43 -0700

    cmopare user input name and display message

--- SayMyNameAgain/Program.cs
+++ SayMyNameAgain/Program.cs
@@ -1,14 +1,30 @@
-﻿namespace SayMyNameAgain
+﻿
+
+namespace SayMyNameAgain
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             string userInput = "";
-            
+
             Console.WriteLine("Please type your name:");
             userInput = Console.ReadLine();
 
+            if (userInput == "Emily" || userInput == "emily" || userInput == "Joe" || userInput == "joe")
+            {
+                Console.WriteLine("Hello, " + userInput + "! Welcome back!");
+            }
+            else if (userInput == "Kyson" || userInput == "kyson")
+            {
+                Console.WriteLine("Your name is awesome! Nice to meet you!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
+
+            Console.Read();
         }
     }
 }
